2e57d2ee8fb7ebdc0e3488641c4c13252c3a3b37
Andrew <andrewurom@gmail.com>
2021-02-14 23:35:42 +0100

    implemented some validations

--- src/app/credit-card/credit-card.component.ts
+++ src/app/credit-card/credit-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms'
 import { CreditCard } from 'src/app/shared/interface/credit-card';
 import { CreditCardService } from '../shared/services/credit-card.service';
 
@@ -12,7 +12,7 @@ import { CreditCardService } from '../shared/services/credit-card.service';
 export class CreditCardComponent implements OnInit {
 
   creditCardForm: FormGroup;
-  creditCard: CreditCard | undefined;
+  creditCard = new CreditCard();
 
   constructor(private formBuilder: FormBuilder, private creditCardService: CreditCardService) {
 
@@ -20,26 +20,29 @@ export class CreditCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.creditCardForm = this.formBuilder.group({
-      creditCardNumber: ['', Validators.required],
-      cardHolder: ['', Validators.required],
-      expirationDate: ['', Validators.required],
-      securityCode: ['', Validators.required],
-      amount: ['', Validators.required]
+      creditCardNumber:  new FormControl(this.creditCard.creditCardNumber, Validators.required),
+      cardHolder: new FormControl( this.creditCard.cardHolder, Validators.required),
+      expirationDate: new FormControl(this.creditCard.expirationDate, Validators.required),
+      securityCode: new FormControl(this.creditCard.securityCode, Validators.required),
+      amount: new FormControl(this.creditCard.amount, Validators.required)
   })
   }
 
   onSubmit() {
-    this.creditCard = {
-      creditCardNumber: this.creditCardForm?.get('creditCardNumber')?.value,
-      cardHolder: this.creditCardForm?.get('cardHolder')?.value,
-      expirationDate: this.creditCardForm?.get('expirationDate')?.value,
-      securityCode: this.creditCardForm?.get('securityCode')?.value,
-      amount: this.creditCardForm?.get('amount')?.value,
-    }
+
+    this.creditCard.amount = this.creditCardForm?.get('amount')?.value;
+    this.creditCard.cardHolder = this.creditCardForm?.get('cardHolder')?.value;
+    this.creditCard.expirationDate = this.creditCardForm?.get('expirationDate')?.value;
+    this.creditCard.securityCode = this.creditCardForm?.get('securityCode')?.value;
+    this.creditCard.creditCardNumber = this.creditCardForm?.get('creditCardNumber')?.value;
     //console.log(this.creditCard);
     this.creditCardService.postCreditCard(this.creditCard).subscribe(res => {
-      
+
     })
+  }
+
+  get form() {
+    return this.creditCardForm.controls;
   }
 
 
--- src/app/shared/interface/credit-card.ts
+++ src/app/shared/interface/credit-card.ts
@@ -8,10 +8,56 @@ export interface ICreditCard{
 
 
 export class CreditCard{
-  /**
-   *
-   */
-  constructor() {
+  private _creditCardNumber: number;
+  private _cardHolder: string;
+  private _expirationDate : Date;
+  private _securityCode : number;
+  private _amount : number;
+
+
+  constructor( ) {
 
   }
+
+
+
+  public get creditCardNumber() : number {
+    return this._creditCardNumber;
+  }
+  public set creditCardNumber(v : number) {
+    this._creditCardNumber = v;
+  }
+
+
+  public get cardHolder() : string {
+    return this._cardHolder;
+  }
+  public set cardHolder(v : string) {
+    this._cardHolder = v;
+  }
+
+
+  public get expirationDate() : Date {
+    return this._expirationDate;
+  }
+  public set expirationDate(v : Date) {
+    this._expirationDate = v;
+  }
+
+
+  public get securityCode() : number {
+    return this._securityCode;
+  }
+  public set securityCode(v : number) {
+    this._securityCode = v;
+  }
+
+
+  public get amount() : number {
+    return this._amount;
+  }
+  public set amount(v : number) {
+    this._amount = v;
+  }
+
 }
